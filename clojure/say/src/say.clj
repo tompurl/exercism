301612a81471ab99e2d3f875aa67b3fb1fa44894
Tom Purl <tom@tompurl.com>
2022-01-12 09:48:42 -0600

    x

--- clojure/say/src/say.clj
+++ clojure/say/src/say.clj
@@ -33,19 +33,13 @@
    {:num 70 :word "seventy"}
    {:num 80 :word "eighty"}
    {:num 90 :word "ninety"}
-   {:num 100 :word "hundred"}])
+   {:num 100 :word "hundred"}
+   {:num 1000 :word "thousand"}
+   {:num 1000000 :word "million"}
+   {:num 1000000000 :word "billion"}])
 
 (defn get-word [num num-map]
   (:word (first(filter #(= (:num %) num) num-map))))
-
-;; (defn number [num]
-;;   (cond
-;;     (> num 20) (let [multiplier-of-ten (* (get-multiplier-of-ten num) 10)
-;;                      remainder (- num multiplier-of-ten)]
-;;                  (apply str [(get-word multiplier-of-ten) "-" (get-word remainder)]))
-;;     :else
-;;     (get-word num)))
-
 
 (defn get-one-or-two-digit-number [num]
   (let [tens-num (* (int (/ num 10)) 10)
@@ -65,8 +59,35 @@
         (> remainder 0) (apply str [hundred-phrase " " (get-one-or-two-digit-number remainder)])
         hundred-phrase)))
 
+(defn get-small-number [num]
+  "Return the string value of a number less than 1000"
+  (if (and (>= num 0) (< num 100))
+    (get-one-or-two-digit-number num)
+    (get-three-digit-number num)))
+
 (defn number [num]
-  ;; FIXME - Only works up to 999
-  (cond
-    (and (>= num 0) (< num 100)) (get-one-or-two-digit-number num)
-    (and (>= num 100) (< num 1000)) (get-three-digit-number num)))
+  (loop [num num
+         multiplier 1
+         phrase ""]
+    (cond
+      (and (>= num 0) (< num 1000)) (clojure.string/trim (apply str
+                                                                [(get-small-number num)
+                                                                 " "
+                                                                 (get-word multiplier multiples-of-ten)
+                                                                 " "
+                                                                 phrase]))
+      :else
+      (recur
+       (int (/ num 1000))
+       (* 1000 multiplier)
+       (if (= (mod num 1000) 0)
+         (apply str [phrase " " (get-word multiplier multiples-of-ten)])
+         (apply str [phrase " " (get-small-number (mod num 1000)) " " (get-word multiplier multiples-of-ten)])))
+      )
+    )
+  )
+
+;; num     multiplier    phrase
+;; ------- ------------- -----------
+;; 1000    1             ""
+;; 1       1000         nil one nil                                        ;;
